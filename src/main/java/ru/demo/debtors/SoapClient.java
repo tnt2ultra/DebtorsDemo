ccf656de409c1fc7686bacb8a1e8ff91cd1d69bc
tnt2ultra <tnt2ultra@ya.ru>
2020-12-21 09:30:13 +0300

    add SoapActionCallback

--- src/main/java/ru/demo/debtors/SoapClient.java
+++ src/main/java/ru/demo/debtors/SoapClient.java
@@ -5,6 +5,7 @@ import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.ws.client.core.WebServiceTemplate;
+import org.springframework.ws.soap.client.core.SoapActionCallback;
 import org.springframework.ws.transport.http.HttpComponentsMessageSender;
 
 import ru.demo.debtors.soap.bindings.GetDebtorByIdBankrupt;
@@ -24,7 +25,8 @@ public class SoapClient {
 		WebServiceTemplate webServiceTemplate = new WebServiceTemplate(jaxb2Marshaller);
 		webServiceTemplate.setMessageSender(httpComponentsMessageSender);
 		return (GetDebtorByIdBankruptResponse) webServiceTemplate.marshalSendAndReceive(
-				"https://services.fedresurs.ru/Bankruptcy/MessageServiceDemo/WebService.svc", itemRequest);
+				"https://services.fedresurs.ru/Bankruptcy/MessageServiceDemo/WebService.svc", itemRequest,
+				 new SoapActionCallback("http://tempuri.org/IMessageService/GetDebtorByIdBankrupt"));
 	}
 
 }
